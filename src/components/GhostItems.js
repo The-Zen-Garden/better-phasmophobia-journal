c0600310ac7fdfd7194e3e23c1ff8b9d6ec51696
Ryne <ryne@itsrynenotryan.com>
2022-02-19 02:09:06 -0700

    Sort ghosts logic

--- src/components/GhostItems.js
+++ src/components/GhostItems.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import { ghosts } from '../data/ghosts';
 
-const ghostItems = ghosts.map((ghost) => (
-  <div className="ghost {ghost.name}">{ghost.name}</div>
-));
+const ghostItems = ghosts
+  .sort((a, b) => {
+    let name1 = a.name.toUpperCase(),
+      name2 = b.name.toUpperCase();
+    return name1 === name2 ? 0 : name1 > name2 ? 1 : -1;
+  })
+  .map((ghost) => (
+    <div
+      key={ghost.name.toLowerCase()}
+      className={`ghost ${ghost.name.toLowerCase()} ${
+        ghost.excluded !== false ? 'disabled' : ''
+      }`}
+    >
+      <h2>{ghost.name}</h2>
+      <div className="actions">
+        <a href="#!" title="Ghost Info">
+          <span>
+            <i className="gg-info"></i>
+          </span>
+        </a>
+        <a href="#!" title="Exclude Ghost">
+          <span>
+            <i className="gg-close-o"></i>
+          </span>
+        </a>
+      </div>
+    </div>
+  ));
 
 class GhostItems extends React.Component {
   render() {
