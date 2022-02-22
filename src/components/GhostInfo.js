7ab53f824ee0dfaf2683c294c6e637b43cde9164
Ryne <ryne@itsrynenotryan.com>
2022-02-21 23:41:58 -0700

    Ghost Info component fields

--- src/components/GhostInfo.js
+++ src/components/GhostInfo.js
@@ -55,48 +55,24 @@ export default class GhostInfo extends Component {
           <div className="s_w">
             <div className="strength">
               <h3>Strength</h3>
-              <p>Can throw multiple objects at once</p>
+              <p>{this.props.ghostList[this.props.ghostIndex].strength}</p>
             </div>
             <div className="weakness">
               <h3>Weakness</h3>
-              <p>Ineffective in empty rooms with no throwables</p>
+              <p>{this.props.ghostList[this.props.ghostIndex].weakness}</p>
             </div>
             <div className="abilities">
               <h3>Abilities</h3>
-              <p>Decrease sanity by throwing objects</p>
+              <p>{this.props.ghostList[this.props.ghostIndex].abilities}</p>
             </div>
           </div>
           <div className="behavior">
             <h3>Behavior</h3>
-            <p>
-              When a Poltergeist interacts with the environment, it has a chance
-              of throwing multiple items at once. If a player is nearby, it will
-              decrease their sanity by an amount equal to the number of items
-              thrown multiplied by 2%. This will create an EMF level 3 reading.
-              Poltergeists can also throw items with higher force than other
-              ghosts.
-            </p>
-            <p>
-              If there are no items near a Poltergeist, it is unable to use its
-              ability. This, however, will not prevent a hunt.
-            </p>
+            <p>{this.props.ghostList[this.props.ghostIndex].behavior}</p>
           </div>
           <div className="strategies">
             <h3>Strategies</h3>
-            <p>
-              A Poltergeist's ability to manipulate multiple objects at once may
-              make it easier to identify. Players may throw multiple objects in
-              close proxmity and observe whether the ghost throws multiple items
-              at once.
-            </p>
-            <p>
-              Unlike other ghosts, objects thrown by Poltergeists have the
-              ability to decrease player sanity, even when they are not looking
-              at the objects being thrown. This can also help to identify the
-              ghost; if a player notices a dip in their sanity after they hear
-              physical disturbances in nearby rooms, they can guess it is likely
-              a Poltergeist they are facing.
-            </p>
+            <p>{this.props.ghostList[this.props.ghostIndex].strategies}</p>
           </div>
         </div>
         <a id="back_button" href="#!">
